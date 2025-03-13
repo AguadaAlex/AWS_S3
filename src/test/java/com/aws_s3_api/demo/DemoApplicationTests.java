c21346a1424ef9ea121fb12713c7061ea947f13c
aaguada <alex.aguada@snoopconsulting.com>
2025-03-13 17:07:02 -0300

    iniciando proyecto

---
+++ src/test/java/com/aws_s3_api/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws_s3_api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
